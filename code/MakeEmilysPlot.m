128b5922a00f650395eb4d06aada489c7560c9ff
andypicke <andypicke@gmail.com>
2016-10-17 16:38:27 -0600

    update w/ new 2d hist and binned plots
    Older scatter plots colored by gamma had too many points and it was
    difficult to see the true patterns

--- code/MakeEmilysPlot.m
+++ code/MakeEmilysPlot.m
@@ -4,6 +4,11 @@
 %
 % Can you make a scatter plot with N2 (or dTdz) on one axis, log10(chi) on a second axis, and color by the gamma (or log10 gamma) needed to have eps_chi=eps? Sorry I am stuck on the stratification piece. (Patches clumping so strongly at 1 m does seem like it might be problematic.)
 %
+% * Note: I think the scatter plots are misleading, because there are too
+% many points. See the 2D histograms further down, these should be more
+% accurate.
+%
+%
 %-------------------
 %  10/05/16 - A.Pickering - andypicke@gmail.com
 %~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -36,120 +41,120 @@ gam_cham=n2 .* chi ./2 ./ eps ./ (dtdz.^2);
 
 
 %% Plot N2 vs chi, color by gamma ?
+% 
+% % There's too many points and they are being overplotted, try taking a
+% % random subsample to plot
+% ir=randi(numel(n2),[round(0.05*numel(n2)),1] );
+% %ir=randi(numel(n2),[40000,1] );
+% 
+% close all
+% figure(1);clf
+% set(gca,'DefaultAxesFontsize',15)
+% %h=scatter( log10(n2(:)), log10(chi(:)), 10, log10(gam_cham(:)),'o', 'filled' ) 
+% h=scatter( log10(n2(ir)), log10(chi(ir)), 10, log10(gam_cham(ir)),'o', 'filled' ) 
+% 
+% % Make transparent (note only works in vers >= 2015b)
+% h.MarkerFaceAlpha=0.2
+% 
+% xlabel('log_{10}N^2','fontsize',16)
+% ylabel('log_{10}\chi','fontsize',16)
+% xlim([-6 -2.5])
+% ylim([-12 -4])
+% grid on
+% hc=colorbar;
+% hc.Label.String='log_{10}\Gamma';
+% hc.Label.FontSize=14;
+% caxis([-2.5 -0.5])
+% 
+% 
+% %%
+% 
+% figdir='/Users/Andy/Cruises_Research/ChiPod/Cham_Eq14_Compare/mfiles/Patches'
+% print( fullfile( figdir,'N2vsChi_byGamma'), '-dpng')
+% 
+% %% Plot N2 vs Epsilon, colored by gamma
+% close all
+% figure(1);clf
+% %h=scatter( log10(n2(:)), log10(eps(:)), 10, log10(gam_cham(:)), 'o', 'filled' ) 
+% h=scatter( log10(n2(ir)), log10(eps(ir)), 10, log10(gam_cham(ir)), 'o', 'filled' ) 
+% h.MarkerFaceAlpha=0.2
+% xlabel('log_{10}N^2','fontsize',16)
+% ylabel('log_{10}\epsilon','fontsize',16)
+% xlim([-6 -2.5])
+% ylim([-8 -4])
+% grid on
+% hc=colorbar;
+% hc.Label.String='log_{10}\Gamma';
+% hc.Label.FontSize=14;
+% %caxis([-2.25 -.85])
+% caxis([-2.5 -0.5])
+% %colormap(jet)
+% 
+% figdir='/Users/Andy/Cruises_Research/ChiPod/Cham_Eq14_Compare/mfiles/Patches'
+% print( fullfile( figdir,'N2vsEps_byGamma'), '-dpng')
+% 
+% %%
+% 
+% figure(3);clf
+% h=scatter(log10(eps),log10(gam_cham),'.','MarkerFaceAlpha',0.25)
+% xlabel('\epsilon','fontsize',16)
+% ylabel('\Gamma','fontsize',16)
+% grid on
+% ylim([-5 1])
+% %%
+% figure(5);clf
+% histogram2(log10(eps),log10(gam_cham),200,'DisplayStyle','tile')
+% xlabel('log_{10}\epsilon','fontsize',16)
+% ylabel('log_{10}\Gamma','fontsize',16)
+% colorbar
+% ylim([-5 2])
+% xlim([-8 -4])
+% title('2D hist')
+% 
+% %%
+% 
+% figure(4);clf
+% h=scatter(log10(chi),log10(gam_cham),'.','MarkerFaceAlpha',0.25)
+% xlabel('\chi','fontsize',16)
+% ylabel('\Gamma','fontsize',16)
+% grid on
+% ylim([-5 1])
+% 
+% 
+% %%
+% figure(5);clf
+% histogram2(log10(chi),log10(gam_cham),200,'DisplayStyle','tile')
+% xlabel('log_{10}\chi','fontsize',16)
+% ylabel('log_{10}\Gamma','fontsize',16)
+% colorbar
+% ylim([-6 3])
+% xlim([-13 -3])
+% title('2D hist')
+% 
+% %%
+% 
+% figure(5);clf
+% histogram2(real(log10(n2)),log10(gam_cham),200,'DisplayStyle','tile')
+% xlabel('log_{10}N^2','fontsize',16)
+% ylabel('log_{10}\Gamma','fontsize',16)
+% colorbar
+% ylim([-6 3])
+% xlim([-5.5 -3])
+% title('2D hist')
+% 
+% %%
+% 
+% figure(5);clf
+% histogram2(real(log10(dtdz)),log10(gam_cham),200,'DisplayStyle','tile')
+% xlabel('log_{10}dT/dz','fontsize',16)
+% ylabel('log_{10}\Gamma','fontsize',16)
+% colorbar
+% ylim([-6 3])
+% xlim([-4 -0])
+% title('2D hist')
+% 
 
-% There's too many points and they are being overplotted, try taking a
-% random subsample to plot
-ir=randi(numel(n2),[round(0.05*numel(n2)),1] );
-%ir=randi(numel(n2),[40000,1] );
-
-close all
-figure(1);clf
-set(gca,'DefaultAxesFontsize',15)
-%h=scatter( log10(n2(:)), log10(chi(:)), 10, log10(gam_cham(:)),'o', 'filled' ) 
-h=scatter( log10(n2(ir)), log10(chi(ir)), 10, log10(gam_cham(ir)),'o', 'filled' ) 
-
-% Make transparent (note only works in vers >= 2015b)
-h.MarkerFaceAlpha=0.2
-
-xlabel('log_{10}N^2','fontsize',16)
-ylabel('log_{10}\chi','fontsize',16)
-xlim([-6 -2.5])
-ylim([-12 -4])
-grid on
-hc=colorbar;
-hc.Label.String='log_{10}\Gamma';
-hc.Label.FontSize=14;
-caxis([-2.5 -0.5])
-
-
-%%
-
-figdir='/Users/Andy/Cruises_Research/ChiPod/Cham_Eq14_Compare/mfiles/Patches'
-print( fullfile( figdir,'N2vsChi_byGamma'), '-dpng')
-
-%% Plot N2 vs Epsilon, colored by gamma
-close all
-figure(1);clf
-%h=scatter( log10(n2(:)), log10(eps(:)), 10, log10(gam_cham(:)), 'o', 'filled' ) 
-h=scatter( log10(n2(ir)), log10(eps(ir)), 10, log10(gam_cham(ir)), 'o', 'filled' ) 
-h.MarkerFaceAlpha=0.2
-xlabel('log_{10}N^2','fontsize',16)
-ylabel('log_{10}\epsilon','fontsize',16)
-xlim([-6 -2.5])
-ylim([-8 -4])
-grid on
-hc=colorbar;
-hc.Label.String='log_{10}\Gamma';
-hc.Label.FontSize=14;
-%caxis([-2.25 -.85])
-caxis([-2.5 -0.5])
-%colormap(jet)
-
-figdir='/Users/Andy/Cruises_Research/ChiPod/Cham_Eq14_Compare/mfiles/Patches'
-print( fullfile( figdir,'N2vsEps_byGamma'), '-dpng')
-
-%%
-
-figure(3);clf
-h=scatter(log10(eps),log10(gam_cham),'.','MarkerFaceAlpha',0.25)
-xlabel('\epsilon','fontsize',16)
-ylabel('\Gamma','fontsize',16)
-grid on
-ylim([-5 1])
-%%
-figure(5);clf
-histogram2(log10(eps),log10(gam_cham),200,'DisplayStyle','tile')
-xlabel('log_{10}\epsilon','fontsize',16)
-ylabel('log_{10}\Gamma','fontsize',16)
-colorbar
-ylim([-5 2])
-xlim([-8 -4])
-title('2D hist')
-
-%%
-
-figure(4);clf
-h=scatter(log10(chi),log10(gam_cham),'.','MarkerFaceAlpha',0.25)
-xlabel('\chi','fontsize',16)
-ylabel('\Gamma','fontsize',16)
-grid on
-ylim([-5 1])
-
-
-%%
-figure(5);clf
-histogram2(log10(chi),log10(gam_cham),200,'DisplayStyle','tile')
-xlabel('log_{10}\chi','fontsize',16)
-ylabel('log_{10}\Gamma','fontsize',16)
-colorbar
-ylim([-6 3])
-xlim([-13 -3])
-title('2D hist')
-
-%%
-
-figure(5);clf
-histogram2(real(log10(n2)),log10(gam_cham),200,'DisplayStyle','tile')
-xlabel('log_{10}N^2','fontsize',16)
-ylabel('log_{10}\Gamma','fontsize',16)
-colorbar
-ylim([-6 3])
-xlim([-5.5 -3])
-title('2D hist')
-
-%%
-
-figure(5);clf
-histogram2(real(log10(dtdz)),log10(gam_cham),200,'DisplayStyle','tile')
-xlabel('log_{10}dT/dz','fontsize',16)
-ylabel('log_{10}\Gamma','fontsize',16)
-colorbar
-ylim([-6 3])
-xlim([-4 -0])
-title('2D hist')
-
-
-%%
+%% Plot 2D histograms of gamma vs each variable
 
 figure(1);clf
 agutwocolumn(1)
@@ -219,19 +224,21 @@ subplot(211)
 pcolor(log10(dN(1:end-1)),log10(dchi(1:end-1)),mean_gamma)
 %pcolor(log10(dN(1:end-1)),log10(dchi(1:end-1)),median_gamma)
 shading flat
-caxis([0 0.15])
-colorbar
+caxis([0 0.1])
+cb=colorbar;
+cb.Label.String='\Gamma';
 ylabel('log_{10}\chi','fontsize',16)
 xlabel('log_{10}N^2','fontsize',16)
 grid on
-title('mean gamma')
+title('mean gamma') 
 
 subplot(212)
 %pcolor(log10(dN(1:end-1)),log10(dchi(1:end-1)),mean_gamma)
 pcolor(log10(dN(1:end-1)),log10(dchi(1:end-1)),median_gamma)
 shading flat
-caxis([0 0.15])
-colorbar
+caxis([0 0.11])
+cb=colorbar;
+cb.Label.String='\Gamma';
 ylabel('log_{10}\chi','fontsize',16)
 xlabel('log_{10}N^2','fontsize',16)
 grid on
